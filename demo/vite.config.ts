35ef1be2fa7bb439e42fa8e62470a10c5d8b09ca
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-12-18 13:04:35 +0300

    feat(module): switch to named export

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import loadVersion from '@sws2apps/vite-plugin-package-version';
+import { loadVersion } from '@sws2apps/vite-plugin-package-version';
 
 // https://vitejs.dev/config/
 export default defineConfig({
